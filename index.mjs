31acc6b93ffcc7ae8939b5713e833bcdd442f6ad
nguyendangtu <nguyendangtu1987@gmail.com>
2024-09-01 12:03:35 -0500

    update

--- index.mjs
+++ index.mjs
@@ -1,4 +1,4 @@
-import { send } from "./db";
+import { send } from "./db.mjs";
 import { GetItemCommand, PutItemCommand, DeleteItemCommand, ScanCommand, UpdateItemCommand } from "@aws-sdk/client-dynamodb";
 import { marshall, unmarshall } from "@aws-sdk/util-dynamodb";
 
